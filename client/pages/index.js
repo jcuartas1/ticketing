bf8de4c656f8d93ee4dfc07caf6952d0c2d17a30
jcuartas1 <julianandresc@gmail.com>
2022-05-16 14:32:33 -0500

    changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,8 +3,6 @@ import Link from 'next/link';
 
 const Landing = ({currentUser, tickets}) => {
 
-  console.log(tickets)
-
   const tickeList = tickets.map(ticket => {
     return (
       <tr key={ ticket.id }>
